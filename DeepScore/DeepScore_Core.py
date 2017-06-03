5dceab63a915485951668fb836d55a2e84e25ee3
alivcor <abhinandandubey@live.com>
2017-06-03 19:20:28 -0400

    first run kappa metrics

--- DeepScore/DeepScore_Core.py
+++ DeepScore/DeepScore_Core.py
@@ -21,6 +21,7 @@ import numpy as np
 from keras.models import model_from_yaml
 from keras.models import Sequential
 from keras.layers import Dense, Activation, LSTM
+import DeepScore_Metrics
 # fix random seed for reproducibility
 import pickle
 
@@ -179,9 +180,22 @@ def testModel(model_fn=None):
     EventIssuer.issueExit(_LOGFILENAME, timestamp)
 
 
+def decodeOneHot(rating_array):
+    return rating_array.index(1)
 
 
 def qw_kappa(y_true, y_pred):
+    assert(y_true.shape[0] == y_pred.shape[0])
+    decoded_y_true = np.zeros(y_true.shape[0])
+    decoded_y_pred = np.zeros(y_pred.shape[0])
+    for i in range(0, y_true.shape[0]):
+        decoded_y_true[i] = decodeOneHot(y_true[i])
+        decoded_y_pred[i] = decodeOneHot(y_pred[i])
+    qwk_value = DeepScore_Metrics.quadratic_weighted_kappa(decoded_y_true, decoded_y_pred)
+    return qwk_value
+
+
+
 
 
 def traintest_model():
